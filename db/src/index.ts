3add8f37734bdcd64c6156ffa313677921467931
Arnob-B <arnobbhakta00@gmail.com>
2024-10-19 17:04:19 +0530

    userSeeding and playerSeeding from DB done

--- db/src/index.ts
+++ db/src/index.ts
@@ -2,8 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { createClient } from "redis";
 import dbManager from "./utils/dbManager";
 import { userList } from "./seeding/users";
+import playersList from "./seeding/finalPlayerList";
 
-const seeding=async ()=>{
+const userSeeding=async ()=>{
   const client = new PrismaClient();
   const res = await client.user.count();
   if( res < 200){
@@ -21,7 +22,28 @@ const seeding=async ()=>{
   }
 }
 
+const playerSeeding = async ()=>{
+  const obj = JSON.parse(playersList);
+  const client = new PrismaClient();
+  console.log("checking DB");
+  const res = await client.player.count();
+  if( res < 500){
+    for (let a of obj) {
+      console.log("pushing");
+      await client.player.create({
+        data:{
+          id:a.ID.toString(),
+          name:a.Name,
+          basePrice:a.Price,
+          ownerId: "admin"
+        }
+      });
+    }
+  }
+}
+
 const main = async ()=>{
-  await seeding();
+  await userSeeding();
+  await playerSeeding();
 }
 main();
