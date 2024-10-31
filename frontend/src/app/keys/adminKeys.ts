fd12b9b7764ebe4ef6041d035e48ec070afd3365
rahul-p19 <rahulpandey.dps@gmail.com>
2024-10-31 14:03:09 +0530

    updated keys

--- frontend/src/app/keys/adminKeys.ts
+++ frontend/src/app/keys/adminKeys.ts
@@ -1,2 +1,2 @@
 export const adminApi = "http://auckipl.duckdns.org/api/admin"
-export const adminWsApi = "http://auckipl.duckdns.org/api/adminWs"
+export const adminWsApi = "http://auckipl.duckdns.org/adminWs"
