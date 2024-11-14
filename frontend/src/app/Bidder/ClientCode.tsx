52bb483d1943c159fd881e274200b6cc4aaa0550
Arnob-B <arnobbhakta00@gmail.com>
2024-11-15 01:34:25 +0530

    frontend wsClient close function added

--- frontend/src/app/Bidder/ClientCode.tsx
+++ frontend/src/app/Bidder/ClientCode.tsx
@@ -162,7 +162,8 @@ export default function ClientCode({userId, userName}:{userId:string, userName:s
 	}
 
 	useEffect(() => {
-		const main = async () => {
+			const wsClient = new WebSocket(generalWsApi);
+		const main = async (wsClient:WebSocket) => {
 			const res = await fetch(generalApi + "/getCurrentPlayer");
 			const body = await res.json();
 			const data = body.msg;
@@ -182,7 +183,6 @@ export default function ClientCode({userId, userName}:{userId:string, userName:s
 			});
 			setNextBid(data.nextBid);
 
-			const wsClient = new WebSocket(generalWsApi);
 			wsClient.onopen = () =>{
 				setIsLive(()=> true);
 				setInterval(() => {
@@ -265,7 +265,10 @@ export default function ClientCode({userId, userName}:{userId:string, userName:s
 				}
 			};
 		};
-		main();
+		main(wsClient);
+		return(()=>{
+			if(wsClient.readyState) {console.log("here");wsClient.close()};
+		})
 	}, []);
 	// console.log(">_<");
 	// console.log(playerDetails);
