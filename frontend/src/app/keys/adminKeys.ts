ec2c60282dda4113f7498a2f099922296b1587d1
Arnob-B <arnobbhakta00@gmail.com>
2024-11-15 05:29:37 +0530

    frontend | keys updated

--- frontend/src/app/keys/adminKeys.ts
+++ frontend/src/app/keys/adminKeys.ts
@@ -1,2 +1,2 @@
-export const adminApi = "https://backend1.juecell.com/api/admin"
-export const adminWsApi = "https://backend1.juecell.com/adminWs"
+export const adminApi = "https://backend2.juecell.com/api/admin"
+export const adminWsApi = "https://backend2.juecell.com/adminWs"
--- frontend/src/app/keys/generalApi.ts
+++ frontend/src/app/keys/generalApi.ts
@@ -1,2 +1,2 @@
-export const generalApi = "https://backend1.juecell.com/api"
-export const generalWsApi = "https://backend1.juecell.com/generalWs"
+export const generalApi = "https://backend2.juecell.com/api"
+export const generalWsApi = "https://backend2.juecell.com/generalWs"
