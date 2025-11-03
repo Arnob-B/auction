1d5cb754d8db39950fe97db2c5e6ec55989a25ab
Arnob-B <arnobbhakta00@gmail.com>
2025-11-04 00:56:08 +0530

    fix: fix callback url

--- frontend/src/components/UserNavbar.tsx
+++ frontend/src/components/UserNavbar.tsx
@@ -4,19 +4,20 @@ import Link from 'next/link'
 import { signOut } from 'next-auth/react'
 
 function UserNavbar() {
+	const HOME_URL = "https://summit-cup.juecell.com/";
   return (
     <>
     <nav className='absolute hidden top-20 sm:top-10 font-opensans px-6 py-3 text-accent rounded-full text-sm backdrop-blur-md bg-gradient-to-bl from-white/10 via-white/10 to-white/15 sm:flex gap-x-6 transition-colors'>
       <Link href="/Bidder" className='hover:text-primary'>Bidding</Link>
       <Link href="/leaderboard" className='hover:text-primary'>Leaderboard</Link>
       <Link href="/purchases" className='hover:text-primary'>Your Team</Link>
-      <button onClick={()=>signOut({callbackUrl: "/", redirect: true})} className='text-primary hover:text-accent'>Logout</button>
+      <button onClick={()=>signOut({callbackUrl: HOME_URL, redirect: true})} className='text-primary hover:text-accent'>Logout</button>
     </nav>
     <nav className='absolute sm:hidden top-20 sm:top-10 font-opensans px-6 py-3 text-accent rounded-full text-sm backdrop-blur-md bg-gradient-to-bl from-white/10 via-white/10 to-white/15 flex gap-x-6 transition-colors'>
       <Link href="/Bidder" className='hover:text-primary'>Bidding</Link>
       <Link href="/leaderboard" className='hover:text-primary'>Leaderboard</Link>
       <Link href="/purchases" className='hover:text-primary'>Your Team</Link>
-      <button onClick={()=>signOut({callbackUrl: "/", redirect: true})} className='hover:text-primary'>Logout</button>
+      <button onClick={()=>signOut({callbackUrl: HOME_URL, redirect: true})} className='hover:text-primary'>Logout</button>
     </nav>
     </>
   )
