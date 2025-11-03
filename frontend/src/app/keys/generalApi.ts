bbbcaacd0213f526ab7629d9d14d0049ea134618
Arnob-B <arnobbhakta00@gmail.com>
2025-11-03 20:14:17 +0530

    fix: fix frontend

--- frontend/src/app/keys/generalApi.ts
+++ frontend/src/app/keys/generalApi.ts
@@ -1,2 +1,6 @@
-export const generalApi = "https://backend2.juecell.com/api"
-export const generalWsApi = "https://backend2.juecell.com/generalWs"
+// export const generalApi = "https://backend2.juecell.com/api"
+// export const generalWsApi = "https://backend2.juecell.com/generalWs"
+
+// testing
+export const generalApi = "http://localhost:3300"
+export const generalWsApi = "ws://localhost:3002/generalWs"
